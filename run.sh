04b4685fd5b55245e061409b7593a618eb6b7404
relthyg <relthyg@posteo.de>
2019-04-25 06:23:46 +0200

    Preseed the installer

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 [[ ! $UID -eq 0 ]] && echo "You need to be root to run this script." && exit 1
 
+set -eo pipefail
+
 LOCATION="$(dirname $(readlink -f "$0"))"
 
 lwr () {
     PYTHONPATH="$LOCATION/live-wrapper${PYTHONPATH+:$PYTHONPATH}" python -c 'import lwr.run; lwr.run.main()' "$@"
 }
 
-[[ -f env.sh ]] && . env.sh
+[[ -f env.lib.sh ]] && . env.lib.sh
 
 BASE=`grep -v ^# package-lists/base.list | xargs`
 EXTRA=`grep -v ^# package-lists/extra.list | xargs`
@@ -26,8 +28,10 @@ lwr \
     --log="lwr.log" \
     --installer \
     --di-daily \
+    --preseed=preseed.cfg \
     --base_debs="$BASE" \
     --extra="$EXTRA" \
     --firmware="$FIRMWARE" \
     --task="$TASK" \
     --image_output="debian-live-buster-amd64-openbox.iso"
+
